ca25a67af613a046d7c4d30e728d19b5b771a99d
urbluemn <nyaroshko1404@gmail.com>
2024-10-23 21:01:05 +0300

    init: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let brackets = {};
+
+  for(let el of bracketsConfig){
+    brackets[el[0]] = el[1];
+  }
+  for(let el of str) {
+    if(el === stack[stack.length-1] && el === brackets[el]) {
+      stack.pop()
+    } else if (el in brackets) {
+      stack.push(el);
+    } else if (brackets[stack.pop()] !== el) {
+      return false;
+    }
+  }
+
+ return stack.length === 0;
+};
+
+console.log(
+  module.exports("([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))", [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']])
+);
